9b19a84603e8b0f0afae2782941694dc039f03fb
oumi-beep <117539799+oumi-beep@users.noreply.github.com>
2024-08-01 00:18:03 +0100

    Add build files or update project

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './App.jsx';
 import './index.css';  // Optional if you have global styles
 
 ReactDOM.render(<App />, document.getElementById('root'));
