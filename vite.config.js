91cb63f16e73133762a04d90f1f82299ba5b3449
oumi-beep <117539799+oumi-beep@users.noreply.github.com>
2024-08-01 01:55:45 +0100

    Resolved merge conflicts

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-=======
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
->>>>>>> f9048e4b29971901412d45a878ab5e8f458f3a12
 
-// https://vitejs.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react()],
   build: {
     outDir: 'dist', // Output directory for the build
   },
 });
-=======
-  base:"/travel_app_projet/",
-  plugins: [react()],
-})
->>>>>>> f9048e4b29971901412d45a878ab5e8f458f3a12
