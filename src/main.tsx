0636f31419fd383e2676b3aa2dd28dd678d8e04c
oumi-beep <117539799+oumi-beep@users.noreply.github.com>
2024-08-01 02:10:08 +0100

    Update and rename main.jsx to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'; // Assuming you have some global styles
 
+// Render the main App component into the root div
 ReactDOM.render(
   <React.StrictMode>
     <App />
