ab53da4db793ed9e16d8116b8c1cbac417007a60
oumi-beep <117539799+oumi-beep@users.noreply.github.com>
2024-10-02 12:41:46 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
-// vite.config.js
-=======
->>>>>>> 672d1b3d7a423912424e4033eefdefc4439caa05
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-<<<<<<< HEAD
-    plugins: [react()],
-    server: {
-        proxy: {
-            '/api': {
-                target: 'http://localhost:2001',
-                changeOrigin: true,
-                rewrite: (path) => path.replace(/^\/api/, '/api'),
-            },
-        },
-    },
-=======
   plugins: [react()],
   base: "/travel_app_projet/",
   server: {
     host: '0.0.0.0',  // Autorise l'accès externe
     port: process.env.PORT || 3000,  // Utilise le port fourni par Render
+    proxy: {
+      '/api': {
+        target: 'http://localhost:2001',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '/api'),
+      },
+    },
   },
->>>>>>> 672d1b3d7a423912424e4033eefdefc4439caa05
 });
